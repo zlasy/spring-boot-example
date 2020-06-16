030f088bd1c25dcae892c09db61388308bae6808
zlasy <0520.le@gmail.com>
2020-06-16 19:29:28 +0800

    test static execute order

---
+++ spring-boot-common/src/main/java/com/example/springbootcommon/java8/StaticOrderTest.java
@@ -0,0 +1,13 @@
+package com.example.springbootcommon.java8;
+
+public class StaticOrderTest {
+
+    static {
+        i = 0;
+    }
+    static int i = 1;
+
+    public static void main(String[] args) {
+        System.out.println(i);
+    }
+}
