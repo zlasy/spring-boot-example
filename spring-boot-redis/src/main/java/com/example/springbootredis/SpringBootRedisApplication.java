2cd4e00c2e1465a157359810a65df46562fa7183
zlasy <0520.le@gmail.com>
2020-06-16 15:33:36 +0800

    printLoadFactories

--- spring-boot-redis/src/main/java/com/example/springbootredis/SpringBootRedisApplication.java
+++ spring-boot-redis/src/main/java/com/example/springbootredis/SpringBootRedisApplication.java
@@ -2,12 +2,41 @@ package com.example.springbootredis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.UrlResource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
+import org.springframework.util.StringUtils;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.Map;
+import java.util.Properties;
 
 @SpringBootApplication
 public class SpringBootRedisApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         SpringApplication.run(SpringBootRedisApplication.class, args);
+        printLoadResource();
+    }
+
+    private static void printLoadResource() throws IOException {
+        Enumeration<URL> urls = ClassLoader.getSystemResources("META-INF/spring.factories");
+//        System.out.println(Collections.list(urls));
+        while (urls.hasMoreElements()) {
+            URL url = urls.nextElement();
+            System.out.println(url.getPath());
+            UrlResource resource = new UrlResource(url);
+            Properties properties = PropertiesLoaderUtils.loadProperties(resource);
+            for (Map.Entry<?, ?> entry : properties.entrySet()) {
+                String factoryClassName = ((String) entry.getKey()).trim();
+                System.out.println(factoryClassName);
+                for (String factoryName : StringUtils.commaDelimitedListToStringArray((String) entry.getValue())) {
+                    System.out.println(factoryName.trim());
+                }
+            }
+        }
     }
 
 }
