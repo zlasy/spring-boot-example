6536963168942b9a24ec88fde7a28e2945eba0dc
zlasy <0520.le@gmail.com>
2020-06-23 09:44:14 +0800

    keyword doc

---
+++ spring-boot-common/src/main/java/com/example/springbootcommon/java8/keyword/ReentrantLockTest.java
@@ -0,0 +1,42 @@
+package com.example.springbootcommon.java8.keyword;
+
+import java.util.ArrayList;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ReentrantLockTest {
+    private ArrayList<Integer> arrayList = new ArrayList<Integer>();
+    private Lock lock = new ReentrantLock();
+    public static void main(String[] args)  {
+        final ReentrantLockTest test = new ReentrantLockTest();
+
+        new Thread(){
+            @Override
+            public void run() {
+                test.insert(Thread.currentThread());
+            };
+        }.start();
+
+        new Thread(){
+            @Override
+            public void run() {
+                test.insert(Thread.currentThread());
+            };
+        }.start();
+    }
+
+    public void insert(Thread thread) {
+        lock.lock();
+        try {
+            System.out.println(thread.getName()+"得到了锁");
+            for(int i=0;i<5;i++) {
+                arrayList.add(i);
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+        }finally {
+            lock.unlock();
+            System.out.println(thread.getName()+"释放了锁");
+        }
+    }
+}
