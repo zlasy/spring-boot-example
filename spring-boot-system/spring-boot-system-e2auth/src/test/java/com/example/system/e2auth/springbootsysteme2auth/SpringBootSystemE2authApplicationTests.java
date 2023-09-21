429e49394ba88ebb690f9a49e3db91d81765d6f9
zhangle <0520.le@gmail.com>
2023-09-21 10:26:17 +0800

    login

---
+++ spring-boot-system/spring-boot-system-e2auth/src/test/java/com/example/system/e2auth/springbootsysteme2auth/SpringBootSystemE2authApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.system.e2auth.springbootsysteme2auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSystemE2authApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
