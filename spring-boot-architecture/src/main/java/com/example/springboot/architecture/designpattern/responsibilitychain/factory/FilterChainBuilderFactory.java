f798bd1b47d1c13b8a7af912c5e7c126f52b3a8a
zhangle <0520.le@gmail.com>
2024-07-05 14:24:30 +0800

    login

--- spring-boot-architecture/src/main/java/com/example/springboot/architecture/designpattern/responsibilitychain/factory/FilterChainBuilderFactory.java
+++ spring-boot-architecture/src/main/java/com/example/springboot/architecture/designpattern/responsibilitychain/factory/FilterChainBuilderFactory.java
@@ -5,17 +5,34 @@ import com.example.springboot.architecture.designpattern.responsibilitychain.imp
 import com.example.springboot.architecture.designpattern.responsibilitychain.impl.SpecialDeliverFilterChainBuilder;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
 @Service
 public class FilterChainBuilderFactory {
 
+    private final ConcurrentMap<String, FilterChainBuilder> cachedBuilders = new ConcurrentHashMap<>();
+
     public FilterChainBuilder getFilterChainBuilder(String type) {
+        if (!cachedBuilders.containsKey(type)) {
+            synchronized (FilterChainBuilder.class) {
+                if (!cachedBuilders.containsKey(type)) {
+                    FilterChainBuilder builder = createFilterChainBuilder(type);
+                    cachedBuilders.put(type, builder);
+                }
+            }
+        }
+        return cachedBuilders.get(type);
+    }
+
+    private FilterChainBuilder createFilterChainBuilder(String type) {
         switch (type) {
             case "special":
                 return new SpecialDeliverFilterChainBuilder();
             case "default":
                 return new DefaultFilterChainBuilder();
             default:
-                return null;
+                throw new IllegalArgumentException("Unsupported type: " + type);
         }
     }
 
